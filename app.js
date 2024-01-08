a6255d5b261d0b376d4ed64ed3e6f257a49f4772
Miguel Maletzke de Avelar - EST <miguelitomatzk.avelar@gmail.com>
2024-01-08 11:33:29 -0300

    Finishing

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
-    const randomNumber=2;
+    const randomNumber = getRandomNum();
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 });
 
+function getRandomNum() {
+    return Math.floor(Math.random()*colors.length);
+}
